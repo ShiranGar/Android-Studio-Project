ca64ed51782f235b63f89b16726fd59cdf576dd8
Shiran <shiran@alumnos.upm.es>
2024-03-27 19:23:45 +0300

    with changes

--- app/src/main/java/com/example/finalappproject/Fragments/StudentCategoriesFragment.java
+++ app/src/main/java/com/example/finalappproject/Fragments/StudentCategoriesFragment.java
@@ -1,4 +1,4 @@
-package com.example.finalappproject;
+package com.example.finalappproject.Fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.finalappproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
